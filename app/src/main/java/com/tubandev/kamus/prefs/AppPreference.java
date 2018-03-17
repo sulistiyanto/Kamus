ce1c32afbd3047643da6abd7dd4c7ad8b667ead8
Sulistiyanto <sulistiyanto404@gmail.com>
2018-03-17 17:33:56 +0700

    added app preference

---
+++ app/src/main/java/com/tubandev/kamus/prefs/AppPreference.java
@@ -0,0 +1,35 @@
+package com.tubandev.kamus.prefs;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.tubandev.kamus.R;
+
+/**
+ * Created by sulistiyanto on 17/03/18.
+ */
+
+public class AppPreference {
+
+    SharedPreferences prefs;
+    Context context;
+
+    public AppPreference(Context context) {
+        prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        this.context = context;
+    }
+
+    public void setFirstRun(Boolean input){
+
+        SharedPreferences.Editor editor = prefs.edit();
+        String key = context.getResources().getString(R.string.app_first_run);
+        editor.putBoolean(key,input);
+        editor.commit();
+    }
+
+    public Boolean getFirstRun(){
+        String key = context.getResources().getString(R.string.app_first_run);
+        return prefs.getBoolean(key, true);
+    }
+}
